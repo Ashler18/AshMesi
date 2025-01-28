ef3a0f05212dab5c23dc02d5df64c20b5732572d
Abdelhadi Alioui <abdelhadialioui2003@gmail.com>
2025-01-28 12:50:14 +0100

    Ajout de la page vendre

---
+++ ashler1/ashler/frontend/src/app/ajouter-voiture/ajouter-voiture.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouter-voiture',
+  imports: [],
+  templateUrl: './ajouter-voiture.component.html',
+  styleUrl: './ajouter-voiture.component.css'
+})
+export class AjouterVoitureComponent {
+
+}
--- ashler1/ashler/frontend/src/app/app.routes.ts
+++ ashler1/ashler/frontend/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { VoitureDetailComponent } from './voiture-detail/voiture-detail.component';
 import { ProfilComponent } from './profil/profil.component';
-
+import { AjouterVoitureComponent } from './ajouter-voiture/ajouter-voiture.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'voiture-detail/:id', component: VoitureDetailComponent },
   { path: 'profil/:id', component: ProfilComponent },
   { path: 'profil', component: ProfilComponent },
+  { path: 'ajouter-voiture', component : AjouterVoitureComponent}
 ];
 
 @NgModule({
